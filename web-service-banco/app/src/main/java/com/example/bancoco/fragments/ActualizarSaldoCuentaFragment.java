7eb85d74d0b8f6a7db6e6fb65671e9955e14c1fd
Cristian Ospina <crisospina523@gmail.com>
2019-09-11 01:08:34 -0500

    [add] new logic for update the accounts

--- web-service-banco/app/src/main/java/com/example/bancoco/fragments/ActualizarSaldoCuentaFragment.java
+++ web-service-banco/app/src/main/java/com/example/bancoco/fragments/ActualizarSaldoCuentaFragment.java
@@ -10,8 +10,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,26 +22,41 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.bancoco.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ActualizarSaldoCuentaFragment extends Fragment {
 
+    // Recibir id de user logueado de la actividad MenuActivity.
+    public final static String ident = "ident";
+
     private Button btnNuevoSaldo;
-    private EditText numeroCuenta, saldoNuevo;
+    private EditText id, saldoNuevo;
+    private RequestQueue mQueue;
+    private Spinner spCuenta;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_actualizar_saldo_cuenta,container,false);
-        numeroCuenta = vista.findViewById(R.id.etNumeroCuenta);
+        id = vista.findViewById(R.id.etNumeroCuenta);
         saldoNuevo = vista.findViewById(R.id.etValorNuevo);
         btnNuevoSaldo = vista.findViewById(R.id.btnValorNuevo);
+        spCuenta = vista.findViewById(R.id.spCuenta);
+
+        mQueue = Volley.newRequestQueue(getContext());
 
         btnNuevoSaldo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,11 +68,59 @@ public class ActualizarSaldoCuentaFragment extends Fragment {
         return vista;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Bundle args = getArguments();
+        if (args != null) {
+            id.setText(args.getString(ident));
+        }
+
+        // Spinner con las cuentas asociadas al usuario.
+        cuentasUsuarios();
+    }
+
+    private void cuentasUsuarios(){
+        final String ident = id.getText().toString();
+        String url = "http://192.168.1.74:8089/web-services-banco/cuentasUsuarioId.php/?ident="+ident;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                ArrayList<String> cuentasUser = new ArrayList<>();
+                try {
+                    JSONArray jsonArray = response.getJSONArray("datos");
+
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject data = jsonArray.getJSONObject(i);
+                        String nrocuenta = data.getString("nrocuenta");
+
+                        // Cuentas asociadas al id del usuario logueado.
+                        cuentasUser.add(nrocuenta);
+
+                        //Se crea adaptador para recorrer cuentas
+                        ArrayAdapter lo_adp_tipos = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, cuentasUser);
+                        spCuenta.setAdapter(lo_adp_tipos);
+
+                    }
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
     private void validarIngreso(){
         final String saldo = saldoNuevo.getText().toString();
-        final String numero = numeroCuenta.getText().toString();
 
-        if(saldo.isEmpty() || numero.isEmpty()){
+        if(saldo.isEmpty()){
             Toast.makeText(getContext(), "Campos obligatorios", Toast.LENGTH_SHORT).show();
         } else {
             addMoney();
@@ -66,10 +131,10 @@ public class ActualizarSaldoCuentaFragment extends Fragment {
 
     private void addMoney(){
         final String saldo = saldoNuevo.getText().toString();
-        final String numero = numeroCuenta.getText().toString();
+        final String cuenta = spCuenta.getSelectedItem().toString();
 
-        //String url = "http://192.168.1.74:8089/web-services-banco/insertarSaldo.php";
-		String url = "http://172.16.22.6:8082/banco-php-android/web-service-banco/WEB-SERVICE-PHP/insertarSaldo.php";
+        String url = "http://192.168.1.74:8089/web-services-banco/insertarSaldo.php";
+		//String url = "http://172.16.22.6:8082/banco-php-android/web-service-banco/WEB-SERVICE-PHP/insertarSaldo.php";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
 
             @Override
@@ -89,7 +154,7 @@ public class ActualizarSaldoCuentaFragment extends Fragment {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> parametros = new HashMap<String, String>();
                 parametros.put("saldo", saldo);
-                parametros.put("nrocuenta", numero);
+                parametros.put("nrocuenta", cuenta);
                 return parametros;
             }
         };
@@ -99,7 +164,5 @@ public class ActualizarSaldoCuentaFragment extends Fragment {
 
     private void  limpiarCampos(){
         saldoNuevo.setText("");
-        numeroCuenta.setText("");
     }
-
 }
