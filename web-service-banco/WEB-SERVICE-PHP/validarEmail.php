58e67cd884675df8af523d65eb34ebc150933d46
Cristian Ospina <crisospina523@gmail.com>
2019-09-14 02:54:08 -0500

    [add] expresion regular - email

---
+++ web-service-banco/WEB-SERVICE-PHP/validarEmail.php
@@ -0,0 +1,28 @@
+<?php 
+ include 'conexion.php';
+	if (isset($_REQUEST['email']) && isset($_REQUEST['clave'])) {
+		$email=$_REQUEST['email'];
+	
+    $registros = $cnx->query("select email, clave from cliente where email = '$email'");
+    
+    // Validar si la consulta si contiene data
+		if($registros->num_rows){
+			$row = $registros->fetch_object();
+			$emailUser =  $row->email;
+    }
+
+    // Validación del email
+    function is_valid_email($str) {
+      return (false !== filter_var($str, FILTER_VALIDATE_EMAIL));
+    }
+   
+    if(is_valid_email($emailUser)) { 
+      echo "El email es valido"; 
+    } else { 
+      echo "El email NO es valido"; 
+    }
+  }
+  else {
+		echo "El usuario y la clave son obligatorios";
+	}
+ ?>
